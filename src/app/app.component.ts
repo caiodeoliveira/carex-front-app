1b2cb8e2030d837141198597a865bc98e41e11d6
caiodeoliveira <caio.ribeiro98@outlook.com>
2023-06-01 23:41:02 -0300

    Testing BreakpointObserver Service Responsivity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {
-  }
+  isPhonePortrait: boolean = false;
+  isLargeDevice: boolean = false;
   
+  constructor(private responsive: BreakpointObserver) {}
+  
+  ngOnInit() {
+    this.responsive.observe(Breakpoints.XSmall).subscribe(result => {
+      this.isPhonePortrait = false;
+
+      if(result.matches) {
+        this.isPhonePortrait = true;
+        console.log(`Screens matches ${Breakpoints.XSmall}`)
+      }
+    })
+
+    this.responsive.observe(Breakpoints.Large).subscribe(result => {
+      this.isLargeDevice = false;
+      console.log(`Screens matches ${Breakpoints.Large}`)
+      if(result.matches) {
+      this.isLargeDevice = true;
+      }
+    }) 
+  }
 
 }
 
