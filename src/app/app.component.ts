eb635b87d42ff697575bd3538b49431bae020d4e
caiodeoliveira <caio.ribeiro98@outlook.com>
2023-06-03 20:58:35 -0300

    Testing ways to filter duplicate items in the array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,26 +11,46 @@ export class AppComponent {
   isPhonePortrait: boolean = false;
   isLargeDevice: boolean = false;
   
+  // pt-BR O Array breakPointList será usado para verificar o tipo de dispositivo na responsividade
+  breakpointsList: string[] = []; 
+  arrWithoutDuplicate: string[] = [];
+
   constructor(private responsive: BreakpointObserver) {}
   
   ngOnInit() {
+
     this.responsive.observe(Breakpoints.XSmall).subscribe(result => {
       this.isPhonePortrait = false;
 
       if(result.matches) {
         this.isPhonePortrait = true;
-        console.log(`Screens matches ${Breakpoints.XSmall}`)
+        this.breakpointsList.push('isPhonePortrait')
+        
+        this.arrWithoutDuplicate = this.breakpointsList.filter((element, index) => {
+          return element.indexOf(element) === index;
+        })
+         console.log(`Screens matches ${Breakpoints.XSmall}`);
       }
+
+
+
     })
 
     this.responsive.observe(Breakpoints.Large).subscribe(result => {
       this.isLargeDevice = false;
-      console.log(`Screens matches ${Breakpoints.Large}`)
+
       if(result.matches) {
-      this.isLargeDevice = true;
+        this.isLargeDevice = true;
+        this.breakpointsList.push('isLargeDevice');
+
+        this.arrWithoutDuplicate = this.breakpointsList.filter((element, index) => {
+          return element.indexOf(element) === index;
+        })
+        console.log(`Screens matches ${Breakpoints.Large}`)
+        console.log(this.arrWithoutDuplicate)
       }
     }) 
   }
-
+ // pt-BR Corrigir função de novo array com filtragem. Não está permitindo adicionar um novo item, mesmo que ele seja diferente.
 }
 
