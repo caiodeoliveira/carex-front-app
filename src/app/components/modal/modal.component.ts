c465283675a4386c90d8010d9d21eb18c7bc5559
Caio Ribeiro <Caio.ribeiro@accenture.com>
2023-11-22 14:09:19 -0300

    fontawesome icon component imported

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-modal',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class ModalComponent {
   visible: boolean = true;
+  backIcon = faCircleArrowLeft;
 }
