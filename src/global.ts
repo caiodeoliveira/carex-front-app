50f542cf28f3cbde2dda679086587400d17e8237
c0665319@vale.com <Vl0129#072>
2024-07-29 20:28:14 -0300

    Global data file and objects created

---
+++ src/global.ts
@@ -0,0 +1,27 @@
+export const global = {
+    terapies: {
+            path: {
+                pelvicPhysioterapy: "../../../assets/pelvic_physioterapy.png",
+                obstetricPhysioterapy: "../../../assets/obstetric_physioterapy.png",
+                doulage: "../../../assets/doulage.png",
+                
+                alternatives: {
+                    dryNedling: "../../../assets/dry_nedling_tp_small.png",
+                    earAcupunture: "../../../assets/ear_acupunture_tp_small.png",
+                    myofacialRelease: "../../../assets/myofacial_release_tp_small.png",
+                    suctionCup: "../../../assets/suction_cup_tp_small.png",
+                }
+            },
+            headers: {
+                alternativeTerapies: "Terapias alternativas, agende uma sessão",
+                physioteraphyTerapies: "Agende uma avaliação",
+            },
+            names: {
+                dryNedling: "Acupuntura",
+                earAcupunture: "Auriculoterapia",
+                myofacialRelease: "Liberação Miofacial",
+                suctionCup: "Ventosaterapia",
+            }
+    },
+    professionalDescription: "Me chamo Ana Beatriz, sou formada em fisioterapia e pós graduanda em fisioterapia pélvica e obstetrícia. Essas são as áreas que atuo: Doulagem, liberação miofascial, Auriculoterapia, Ventosaterapia, Dry needling, Fisioterapia pélvica. Agende uma consulta ou avaliação abaixo. Se desejar, clique no botão ao lado para entrar em contato."
+}
