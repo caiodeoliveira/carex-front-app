1e324f1edcfeec17b83c1f7dd5fb126f2b7aafa7
Caio <caio.ribeiro98@outlook.com>
2025-07-25 15:11:10 -0300

    header component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { ModalModule } from './components/modal/modal.module';
 import { DataTableModule } from './components/data-table/data-table.module';
 import { ProgrammingsModule } from './template/programmings/programmings.module';
 import { HomeModule } from './template/home/home.module';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
@@ -38,7 +38,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     DataTableModule,
     HomeModule,
     HttpClientModule,
-    NavbarComponent,
+    HeaderComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
