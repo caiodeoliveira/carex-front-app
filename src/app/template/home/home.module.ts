bf3898e1630fc244848be33d2a2854368fa28b71
Caio <caio.ribeiro98@outlook.com>
2025-07-25 15:10:29 -0300

    header (navbar) component removed

--- src/app/template/home/home.module.ts
+++ src/app/template/home/home.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { DividerModule } from 'primeng/divider';
 import { ButtonComponentModule } from 'src/app/components/button/button.module';
-import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 
 
 @NgModule({
@@ -12,7 +11,6 @@ import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
     CommonModule,
     DividerModule,
     ButtonComponentModule,
-    NavbarComponent,
   ],
   exports: [HomeComponent]
 })
